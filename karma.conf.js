0df6ec727f7ef7d6c07219befaf4cfa95eb44073
mainul35 <mainuls18@gmail.com>
2021-10-05 21:59:57 +0700

    allow empty tests to success the build

--- karma.conf.js
+++ karma.conf.js
@@ -33,6 +33,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    failOnEmptyTestSuite: false,
   });
 };
